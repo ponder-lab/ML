e4c4bee05c76189b2bc3c66a937eba2d27e1fe9a
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-19 17:47:32 -0400

    Changing tf.Tensor test

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
@@ -71,7 +71,7 @@ public class TestTensorflowModel extends TestPythonMLCallGraphShape {
 		testTf2("tf2k.py", "add", 2, 2, 3);
 		testTf2("tf2l.py", "add", 2, 2, 3);
 		testTf2("tf2m.py", "add", 2, 2, 3);
-		testTf2("tf2n.py", "add", 2, 2, 3);
+		testTf2("tf2n.py", "func2", 1, 2);
 		testTf2("tf2o.py", "add", 2, 2, 3);
 	}
 
