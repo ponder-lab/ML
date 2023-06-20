6c1d62f078ff63efc3a4744a4ee5aa4a997124ef
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-20 11:57:02 -0400

    Adding link to TF docs

--- com.ibm.wala.cast.python.test/data/tf2p.py
+++ com.ibm.wala.cast.python.test/data/tf2p.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 def value_index(a,b):
   return a.value_index + b.value_index
 
+# From https://www.tensorflow.org/versions/r2.9/api_docs/python/tf/Graph#using_graphs_directly_deprecated
 g = tf.Graph()
 with g.as_default():
   # Defines operation and tensor in graph
