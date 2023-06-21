481707190c7a2ad8af96cd2cb247ea1d96cc85a1
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-06-21 15:02:41 -0400

    Formatting

--- com.ibm.wala.cast.python.ml/source/com/ibm/wala/cast/python/ml/client/PythonTensorAnalysisEngine.java
+++ com.ibm.wala.cast.python.ml/source/com/ibm/wala/cast/python/ml/client/PythonTensorAnalysisEngine.java
@@ -151,10 +151,10 @@ public class PythonTensorAnalysisEngine extends PythonAnalysisEngine<TensorTypeA
                   || Objects.equal(tensorFlowAPI, "range")) sources.add(src);
               // Double-level APIs.
               else if (Objects.equal(tensorFlowAPI, "uniform")
-                      || Objects.equal(tensorFlowAPI, "gamma")
-                      || Objects.equal(tensorFlowAPI, "normal")
-                      || Objects.equal(tensorFlowAPI, "poisson")
-                      || Objects.equal(tensorFlowAPI, "truncated_normal")){
+                  || Objects.equal(tensorFlowAPI, "gamma")
+                  || Objects.equal(tensorFlowAPI, "normal")
+                  || Objects.equal(tensorFlowAPI, "poisson")
+                  || Objects.equal(tensorFlowAPI, "truncated_normal")) {
                 // Check the next "level".
                 if (tensorFlowAPIQueue.isEmpty())
                   // not expecting this API call.
@@ -186,11 +186,11 @@ public class PythonTensorAnalysisEngine extends PythonAnalysisEngine<TensorTypeA
                 if (tensorFlowAPIQueue.isEmpty())
                   // not expecting this API call.
                   throw new IllegalStateException("Encountered unexpected API call.");
-                    
+
                 tensorFlowAPI = tensorFlowAPIQueue.remove();
 
-                if (Objects.equal(tensorFlowAPI, "sparse") || Objects.equal(tensorFlowAPI, "linalg"))
-                    sources.add(src);
+                if (Objects.equal(tensorFlowAPI, "sparse")
+                    || Objects.equal(tensorFlowAPI, "linalg")) sources.add(src);
               } else if (Objects.equal(tensorFlowAPI, "range")
                   || Objects.equal(tensorFlowAPI, "constant")) {
                 // Check the next "level".
@@ -202,13 +202,13 @@ public class PythonTensorAnalysisEngine extends PythonAnalysisEngine<TensorTypeA
 
                 if (Objects.equal(tensorFlowAPI, "ragged")) sources.add(src);
               } else if (Objects.equal(tensorFlowAPI, "from_nested_row_lengths")
-                      || Objects.equal(tensorFlowAPI, "from_nested_row_splits")
-                      || Objects.equal(tensorFlowAPI, "from_nested_value_rowids")
-                      || Objects.equal(tensorFlowAPI, "from_row_lengths")
-                      || Objects.equal(tensorFlowAPI, "from_row_limits")
-                      || Objects.equal(tensorFlowAPI, "from_row_splits")
-                      || Objects.equal(tensorFlowAPI, "from_row_starts")
-                      || Objects.equal(tensorFlowAPI, "from_value_rowids")) {
+                  || Objects.equal(tensorFlowAPI, "from_nested_row_splits")
+                  || Objects.equal(tensorFlowAPI, "from_nested_value_rowids")
+                  || Objects.equal(tensorFlowAPI, "from_row_lengths")
+                  || Objects.equal(tensorFlowAPI, "from_row_limits")
+                  || Objects.equal(tensorFlowAPI, "from_row_splits")
+                  || Objects.equal(tensorFlowAPI, "from_row_starts")
+                  || Objects.equal(tensorFlowAPI, "from_value_rowids")) {
                 // Check the next "level".
                 if (tensorFlowAPIQueue.isEmpty())
                   // not expecting this API call.
@@ -219,14 +219,14 @@ public class PythonTensorAnalysisEngine extends PythonAnalysisEngine<TensorTypeA
                 if (Objects.equal(tensorFlowAPI, "RaggedTensor")) sources.add(src);
               }
             } else if (ni.getCallSite()
-                    .getDeclaredTarget()
-                    .getName()
-                    .toString()
-                    .equals("read_data")) sources.add(src);
-            }
+                .getDeclaredTarget()
+                .getName()
+                .toString()
+                .equals("read_data")) sources.add(src);
           }
         }
       }
+    }
     return sources;
   }
 
