ebc3b349e1ef9894da459404cc566b48eeb58d96
Tatiana Castro-Vélez <tcastrovelez@gradcenter.cuny.edu>
2023-11-29 16:44:02 -0500

    Adding documentation

--- com.ibm.wala.cast.python.test/data/tf2_test_callbacks.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_callbacks.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 
+# Testing API https://www.tensorflow.org/api_docs/python/tf/distribute/MirroredStrategy#distribute_datasets_from_function
+# Making sure that function `get_dataset` is in the CG
+
 def get_dataset(input_context):
   batch_size = input_context.get_per_replica_batch_size(2)
   return tf.data.Dataset.range(4).batch(batch_size)
--- com.ibm.wala.cast.python.test/data/tf2_test_callbacks2.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_callbacks2.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 
 # Code from https://www.tensorflow.org/api_docs/python/tf/distribute/Strategy#example_usage_2
+# Testing API https://www.tensorflow.org/api_docs/python/tf/distribute/MirroredStrategy#run
+# Making sure that function `replica_fn` is in the CG
+
 strategy = tf.distribute.MirroredStrategy(["GPU:0", "GPU:1"])
 tensor_input = tf.constant(3.0)
 
