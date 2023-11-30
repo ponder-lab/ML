e5d5a3a0923e6b413f2026ad57141600bf025f3d
Raffi Khatchadourian <raffi.khatchadourian@hunter.cuny.edu>
2023-11-30 16:54:09 -0500

    Lose a URL.
    I don't think we need both.

--- com.ibm.wala.cast.python.test/data/tf2_test_callbacks2.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_callbacks2.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 
-# Code from https://www.tensorflow.org/versions/r2.9/api_docs/python/tf/distribute/Strategy#example_usage_2
-# Testing API https://www.tensorflow.org/versions/r2.9/api_docs/python/tf/distribute/MirroredStrategy#run
+# From https://www.tensorflow.org/versions/r2.9/api_docs/python/tf/distribute/Strategy#example_usage_2
 
 strategy = tf.distribute.MirroredStrategy(["GPU:0", "GPU:1"])
 tensor_input = tf.constant(3.0)
