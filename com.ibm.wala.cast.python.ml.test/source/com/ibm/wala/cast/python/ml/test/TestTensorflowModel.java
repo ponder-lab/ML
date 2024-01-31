cf153105ad0d7a794dc683508f38722cf0b3839e
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-01-30 22:44:47 -0500

    Add new test.
    Should fail.

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflowModel.java
@@ -259,6 +259,7 @@ public class TestTensorflowModel extends TestPythonMLCallGraphShape {
     testTf2("tf2_test_add5.py", "f", 1, 1, 2);
     testTf2("tf2_test_add6.py", "f", 1, 1, 2);
     testTf2("multigpu_training.py", "run_optimization", 2, 4, 2, 3);
+    testTf2("multigpu_training.py", "average_gradients", 1, 1, 2);
     testTf2("tf2_test_reduce_mean.py", "f", 1, 1, 2);
     testTf2("tf2_test_reduce_mean.py", "g", 1, 1, 2);
     testTf2("tf2_test_reduce_mean.py", "h", 1, 1, 2);
