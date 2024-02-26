8b2af37b3143f73e0904371639c6a332ba01b580
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-02-26 15:07:38 -0500

    Fix `tf.range()`.
    Add test.

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflow2Model.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflow2Model.java
@@ -1204,6 +1204,12 @@ public class TestTensorflow2Model extends TestPythonMLCallGraphShape {
     test("tf2_test_tf_range.py", "f", 1, 1, 2);
   }
 
+  @Test
+  public void testTFRange2()
+      throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
+    test("tf2_test_tf_range2.py", "f", 1, 1, 2);
+  }
+
   private void test(
       String filename,
       String functionName,
