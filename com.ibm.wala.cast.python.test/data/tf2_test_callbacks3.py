5d778cf9694590b3386a287fa25c010caec89a4f
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-03-05 10:33:53 -0500

    Apply spotless.

--- com.ibm.wala.cast.python.test/data/tf2_test_callbacks3.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_callbacks3.py
@@ -7,13 +7,14 @@ strategy = tf.distribute.MirroredStrategy(["GPU:0", "GPU:1"])
 
 
 def dataset_fn(input_context):
-  batch_size = input_context.get_per_replica_batch_size(global_batch_size)
-  dataset = tf.data.Dataset.from_tensors(([1.], [1.])).repeat(64).batch(16)
-  dataset = dataset.shard(
-      input_context.num_input_pipelines, input_context.input_pipeline_id)
-  dataset = dataset.batch(batch_size)
-  dataset = dataset.prefetch(2)  # This prefetches 2 batches per device.
-  return dataset
+    batch_size = input_context.get_per_replica_batch_size(global_batch_size)
+    dataset = tf.data.Dataset.from_tensors(([1.0], [1.0])).repeat(64).batch(16)
+    dataset = dataset.shard(
+        input_context.num_input_pipelines, input_context.input_pipeline_id
+    )
+    dataset = dataset.batch(batch_size)
+    dataset = dataset.prefetch(2)  # This prefetches 2 batches per device.
+    return dataset
 
 
 dist_dataset = strategy.distribute_datasets_from_function(dataset_fn)
