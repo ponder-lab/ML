b4619f7b3d4ba7078003dfea2cb41107054a218e
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-03-11 19:13:58 -0400

    Sample from datasets.

--- com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflow2Model.java
+++ com.ibm.wala.cast.python.ml.test/source/com/ibm/wala/cast/python/ml/test/TestTensorflow2Model.java
@@ -1085,6 +1085,12 @@ public class TestTensorflow2Model extends TestPythonMLCallGraphShape {
     test("tf2_test_dataset32.py", "f", 1, 1, 2);
   }
 
+  @Test
+  public void testDataset33()
+      throws ClassHierarchyException, IllegalArgumentException, CancelException, IOException {
+    test("tf2_test_dataset33.py", "f", 1, 1, 2);
+  }
+
   /**
    * Test enumerating a dataset (https://github.com/wala/ML/issues/140). The first element of the
    * tuple returned isn't a tensor.
