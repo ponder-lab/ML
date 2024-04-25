f2772b56db26a04fb320ab94ffdaa48f4e7f4ade
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2024-04-24 21:52:18 -0400

    Make logger private.

--- com.ibm.wala.cast.python/source/com/ibm/wala/cast/python/ipa/callgraph/PythonInstanceMethodTrampolineTargetSelector.java
+++ com.ibm.wala.cast.python/source/com/ibm/wala/cast/python/ipa/callgraph/PythonInstanceMethodTrampolineTargetSelector.java
@@ -51,7 +51,7 @@ import java.util.logging.Logger;
 
 public class PythonInstanceMethodTrampolineTargetSelector<T> implements MethodTargetSelector {
 
-  public static final Logger logger =
+  private static final Logger logger =
       Logger.getLogger(PythonInstanceMethodTrampolineTargetSelector.class.getName());
 
   /**
