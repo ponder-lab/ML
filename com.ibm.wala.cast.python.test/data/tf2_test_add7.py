cc1c1914bf50c7afc7858dafb8f19e9e76af6b60
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-07-18 11:07:14 -0400

    Additional assertions.

--- com.ibm.wala.cast.python.test/data/tf2_test_add7.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add7.py
@@ -12,3 +12,6 @@ def add(a, b):
 
 
 c = add(tf.ones([1, 2]), tf.ones([2, 2]))
+
+assert c.shape == (2, 2), f"Expected shape (2, 2), got {c.shape}"
+assert c.dtype == tf.float32, f"Expected dtype float32, got {c.dtype}"
