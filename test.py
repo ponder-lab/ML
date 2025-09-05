ada8ff6142720575bf5220c3e5a400382e6b796b
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-09-05 15:55:31 -0400

    Add test file.

---
+++ test.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+# Shape (2,)
+a = tf.constant([1.0, 3.0])
+print("Shape (2,): ", a)
+
+# Shape (2, 1)
+b = tf.constant([[1.0], [3.0]])
+print("Shape (2, 1): ", b)
+
+# Shape (1, 2)
+c = tf.constant([[1.0, 3.0]])
+print("Shape (1, 2): ", c)
+
+# Shape (2, 3, 4): 2 blocks, each with 3 rows and 4 columns
+d = tf.constant([
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12]
+    ],
+    [
+        [13, 14, 15, 16],
+        [17, 18, 19, 20],
+        [21, 22, 23, 24]
+    ]
+])
+print("Shape (2, 3, 4): ", d)
