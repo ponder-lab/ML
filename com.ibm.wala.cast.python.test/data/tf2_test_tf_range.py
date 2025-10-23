41950e9caf8cd95dcbcc7562c2f472ae504f8cb3
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-10-23 11:15:36 -0400

    Remove assertions for now.

--- com.ibm.wala.cast.python.test/data/tf2_test_tf_range.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_tf_range.py
@@ -12,12 +12,6 @@ limit = 18
 delta = 3
 
 r = tf.range(start, limit, delta)
-assert isinstance(r, tf.Tensor)
-assert r.shape == (5,)
-assert r.dtype == tf.int32
 
 for i in r:
-    assert isinstance(i, tf.Tensor)
-    assert i.dtype == tf.int32
-    assert i.shape == ()
     f(i)
