aed714784e3087a82b531e569f312ed3ceb60a54
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2025-12-19 09:43:41 -0500

    Add assertions.

--- com.ibm.wala.cast.python.test/data/tf2_test_add.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add.py
@@ -6,6 +6,19 @@ def f(a):
 
 
 x = [1, 2, 3, 4, 5]
+assert isinstance(x, list)
+assert all(isinstance(i, int) for i in x)
+assert tf.convert_to_tensor(x).dtype == tf.int32
+assert tf.convert_to_tensor(x).shape == (5,)
+
 y = 1
+assert isinstance(y, int)
+assert tf.convert_to_tensor(y).dtype == tf.int32
+assert tf.convert_to_tensor(y).shape == ()
+
 z = tf.add(x, y)
+assert isinstance(z, tf.Tensor)
+assert z.dtype == tf.int32
+assert z.shape == (5,)
+
 f(z)
