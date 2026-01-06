23ea583b967919219083f6a2e19735f7527d08a5
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-06 09:51:29 -0500

    Add assertions.

--- com.ibm.wala.cast.python.test/data/tf2_test_add54.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add54.py
@@ -5,4 +5,8 @@ def add(a, b):
     return a + b
 
 
-c = add(tf.keras.Input(shape=(32,)), tf.keras.Input(shape=(32,)))
+arg = tf.keras.Input(shape=(32,))
+assert arg.shape == (None, 32)
+assert arg.dtype == tf.float32
+
+c = add(arg, arg)
