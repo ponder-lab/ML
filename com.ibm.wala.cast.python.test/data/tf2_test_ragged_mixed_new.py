f155a32ce94a4e9a1ea011177cd93bcc709b14cb
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-09 09:37:51 -0500

    Add shape and dtype assertions to mixed args test case.

--- com.ibm.wala.cast.python.test/data/tf2_test_ragged_mixed_new.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_ragged_mixed_new.py
@@ -8,6 +8,8 @@ rt1 = tf.RaggedTensor.from_value_rowids(
     [3, 1, 4, 1, 5, 9],
     value_rowids=[0, 0, 0, 0, 2, 2]
 )
+assert rt1.shape.as_list() == [3, None]
+assert rt1.dtype == tf.int32
 
 # Case 2: Positional values, keyword value_rowids, keyword nrows
 rt2 = tf.RaggedTensor.from_value_rowids(
@@ -15,6 +17,8 @@ rt2 = tf.RaggedTensor.from_value_rowids(
     value_rowids=[0, 0, 0, 0, 2, 2],
     nrows=5
 )
+assert rt2.shape.as_list() == [5, None]
+assert rt2.dtype == tf.int32
 
 # Case 3: Positional values, positional value_rowids, keyword nrows
 rt3 = tf.RaggedTensor.from_value_rowids(
@@ -22,5 +26,7 @@ rt3 = tf.RaggedTensor.from_value_rowids(
     [0, 0, 0],
     nrows=3
 )
+assert rt3.shape.as_list() == [3, None]
+assert rt3.dtype == tf.int32
 
 test_ragged_mixed_args_new(rt1, rt2, rt3)
