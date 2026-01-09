48efacb6e5b0868f60c376d22fa592cbd14cb701
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-09 13:18:30 -0500

    Update assertions in `tf2_test_add93.py` and `tf2_test_add94.py` to verify shape and dtype.

--- com.ibm.wala.cast.python.test/data/tf2_test_add94.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add94.py
@@ -10,3 +10,7 @@ c = add(
     tf.RaggedTensor.from_row_splits([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8, 8]),
     RaggedTensor.from_row_splits([2, 3, 7, 17, 8, 19, 2, 6], [0, 4, 4, 7, 8, 8]),
 )
+
+assert isinstance(c, tf.RaggedTensor)
+assert c.shape.as_list() == [5, None]
+assert c.dtype == tf.int32
