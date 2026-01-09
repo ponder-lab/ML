2f9301a9c5d4366e82d2339980c6e5e3e98ab43c
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-09 11:06:00 -0500

    Update tf2_test_add95.py to assert on arguments rather than parameters.
    - Assertions moved to arg1 and arg2 before the call to add().

--- com.ibm.wala.cast.python.test/data/tf2_test_add95.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add95.py
@@ -2,17 +2,18 @@ import tensorflow as tf
 
 
 def add(a, b):
-    assert a.shape == (5, None)
-    assert a.dtype == tf.int32
-    assert b.shape == (5, None)
-    assert b.dtype == tf.int32
     return a + b
 
 
-c = add(
-    tf.RaggedTensor.from_row_starts([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8]),
-    tf.RaggedTensor.from_row_starts([3, 11, 4, 11, 5, 19, 21, 6], [0, 4, 4, 7, 8]),
-)
+arg1 = tf.RaggedTensor.from_row_starts([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8])
+assert arg1.shape == (5, None)
+assert arg1.dtype == tf.int32
+
+arg2 = tf.RaggedTensor.from_row_starts([3, 11, 4, 11, 5, 19, 21, 6], [0, 4, 4, 7, 8])
+assert arg2.shape == (5, None)
+assert arg2.dtype == tf.int32
+
+c = add(arg1, arg2)
 
 assert c.shape == (5, None)
 assert c.dtype == tf.int32
