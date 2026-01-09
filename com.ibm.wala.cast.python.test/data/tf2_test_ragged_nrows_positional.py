8e8cc2d52ccd8ef1b239e7b62b35f62ff19fe8f7
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-09 10:10:51 -0500

    Run `black`.

--- com.ibm.wala.cast.python.test/data/tf2_test_ragged_nrows_positional.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_ragged_nrows_positional.py
@@ -1,14 +1,12 @@
 import tensorflow as tf
 
+
 def test_ragged_nrows_positional(rt):
     pass
 
+
 # Case: Positional values, positional value_rowids, positional nrows
-rt = tf.RaggedTensor.from_value_rowids(
-    [3, 1, 4],
-    [0, 0, 0],
-    3
-)
+rt = tf.RaggedTensor.from_value_rowids([3, 1, 4], [0, 0, 0], 3)
 assert rt.shape.as_list() == [3, None]
 assert rt.dtype == tf.int32
 
