4dd839ca6b940ed544ab719c526fbd78feeb34b7
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-09 13:20:23 -0500

    Update assertions in `tf2_test_add93.py` and `tf2_test_add94.py` to verify shape and dtype on arguments.

--- com.ibm.wala.cast.python.test/data/tf2_test_add94.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add94.py
@@ -3,17 +3,18 @@ from tensorflow.python.ops.ragged.ragged_tensor import RaggedTensor
 
 
 def add(a, b):
-    assert a.shape.as_list() == [5, None]
-    assert a.dtype == tf.int32
-    assert b.shape.as_list() == [5, None]
-    assert b.dtype == tf.int32
     return a + b
 
 
-c = add(
-    tf.RaggedTensor.from_row_splits([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8, 8]),
-    RaggedTensor.from_row_splits([2, 3, 7, 17, 8, 19, 2, 6], [0, 4, 4, 7, 8, 8]),
-)
+t1 = tf.RaggedTensor.from_row_splits([3, 1, 4, 1, 5, 9, 2, 6], [0, 4, 4, 7, 8, 8])
+t2 = RaggedTensor.from_row_splits([2, 3, 7, 17, 8, 19, 2, 6], [0, 4, 4, 7, 8, 8])
+
+assert t1.shape.as_list() == [5, None]
+assert t1.dtype == tf.int32
+assert t2.shape.as_list() == [5, None]
+assert t2.dtype == tf.int32
+
+c = add(t1, t2)
 
 assert isinstance(c, tf.RaggedTensor)
 assert c.shape.as_list() == [5, None]
