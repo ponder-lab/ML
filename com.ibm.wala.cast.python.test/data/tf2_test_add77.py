4eb1fc0667499800545f068416f900be41a8f254
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-01-12 15:49:16 -0500

    Refactor `tf2_test_add76.py` through `tf2_test_add83.py` to extract argument expressions and add shape and dtype assertions.

--- com.ibm.wala.cast.python.test/data/tf2_test_add77.py
+++ com.ibm.wala.cast.python.test/data/tf2_test_add77.py
@@ -5,11 +5,16 @@ def add(a, b):
     return a + b
 
 
-c = add(
-    tensorflow.RaggedTensor.from_nested_row_splits(
-        [3, 1, 4, 1, 5, 9, 2, 6], ([0, 3, 3, 5], [0, 4, 4, 7, 8, 8])
-    ),
-    tensorflow.RaggedTensor.from_nested_row_splits(
-        [13, 1, 4, 1, 15, 9, 2, 16], ([0, 3, 3, 5], [0, 4, 4, 7, 8, 8])
-    ),
+arg1 = tensorflow.RaggedTensor.from_nested_row_splits(
+    [3, 1, 4, 1, 5, 9, 2, 6], ([0, 3, 3, 5], [0, 4, 4, 7, 8, 8])
 )
+assert arg1.shape == (3, None, None)
+assert arg1.dtype == tensorflow.int32
+
+arg2 = tensorflow.RaggedTensor.from_nested_row_splits(
+    [13, 1, 4, 1, 15, 9, 2, 16], ([0, 3, 3, 5], [0, 4, 4, 7, 8, 8])
+)
+assert arg2.shape == (3, None, None)
+assert arg2.dtype == tensorflow.int32
+
+c = add(arg1, arg2)
