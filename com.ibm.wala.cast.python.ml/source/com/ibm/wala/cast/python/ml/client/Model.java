d67161118e7f75ad278aaea2ad133c39abde0377
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-04-06 20:45:43 -0400

    Fix `Model` parameter indexing for positional constructor calls.
    - Remove `SELF` from `Model.Parameters` to align indices with caller-provided positional arguments.
    - Ensures `INPUTS` and `OUTPUTS` map correctly to `use 1` and `use 2` in non-keyword calls like `Model(a, b)`.
    - Fixes `TestTensorflow2Model.testModelAttributes` while maintaining support for keyword calls.

--- com.ibm.wala.cast.python.ml/source/com/ibm/wala/cast/python/ml/client/Model.java
+++ com.ibm.wala.cast.python.ml/source/com/ibm/wala/cast/python/ml/client/Model.java
@@ -30,9 +30,6 @@ public class Model extends TensorGenerator {
 
   /** Parameter positions and names for {@code tf.keras.Model}. */
   protected enum Parameters {
-    /** The model itself. */
-    SELF,
-
     /** The input(s) of the model. */
     INPUTS,
 
