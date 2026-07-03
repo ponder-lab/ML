c488f5ade233005f9247728785f69848ac93c0c6
Raffi Khatchadourian <khatchad@hunter.cuny.edu>
2026-07-03 10:43:57 -0400

    Remove the wala/ML#687 fixtures accidentally swept into the previous commit.

--- com.ibm.wala.cast.python.test/data/importmod_proj/B.py
+++
@@ -1,8 +0,0 @@
-# wala/ML#687 MRE: a sibling script module imported by A-variants.
-import tensorflow as tf
-import tensorflow.keras as keras
-
-
-class Padding2D(keras.layers.Layer):
-    def call(self, x):
-        return tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], mode="symmetric")
